b1936bfc1eccb9149a73512be449ba3f4b86b72d
core23 <mail@core23.de>
2016-04-09 12:17:03 +0200

    Removed useless ROLE_SUPER_ADMIN check in UserAdmin

--- Admin/Model/UserAdmin.php
+++ Admin/Model/UserAdmin.php
@@ -17,7 +17,6 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\UserBundle\Form\Type\SecurityRolesType;
 use Sonata\UserBundle\Form\Type\UserGenderListType;
 
 class UserAdmin extends Admin
@@ -162,17 +161,17 @@ class UserAdmin extends Admin
                 ->end()
                 ->with('Profile')
                     ->add('dateOfBirth', 'sonata_type_date_picker', array(
-                        'years'       => range(1900, $now->format('Y')),
+                        'years' => range(1900, $now->format('Y')),
                         'dp_min_date' => '1-1-1900',
                         'dp_max_date' => $now->format('c'),
-                        'required'    => false,
+                        'required' => false,
                     ))
                     ->add('firstname', null, array('required' => false))
                     ->add('lastname', null, array('required' => false))
                     ->add('website', 'url', array('required' => false))
                     ->add('biography', 'text', array('required' => false))
                     ->add('gender', UserGenderListType::class, array(
-                        'required'           => true,
+                        'required' => true,
                         'translation_domain' => $this->getTranslationDomain(),
                     ))
                     ->add('locale', 'locale', array('required' => false))
@@ -188,38 +187,28 @@ class UserAdmin extends Admin
                     ->add('gplusName', null, array('required' => false))
                 ->end()
             ->end()
-        ;
-
-        if ($this->getSubject() && !$this->getSubject()->hasRole('ROLE_SUPER_ADMIN')) {
-            $formMapper
-                ->tab('Security')
-                    ->with('Status')
-                        ->add('locked', null, array('required' => false))
-                        ->add('expired', null, array('required' => false))
-                        ->add('enabled', null, array('required' => false))
-                        ->add('credentialsExpired', null, array('required' => false))
-                    ->end()
-                    ->with('Groups')
-                        ->add('groups', 'sonata_type_model', array(
-                            'required' => false,
-                            'expanded' => true,
-                            'multiple' => true,
-                        ))
-                    ->end()
-                    ->with('Roles')
-                        ->add('realRoles', SecurityRolesType::class, array(
-                            'label'    => 'form.label_roles',
-                            'expanded' => true,
-                            'multiple' => true,
-                            'required' => false,
-                        ))
-                    ->end()
-                ->end()
-            ;
-        }
-
-        $formMapper
             ->tab('Security')
+                ->with('Status')
+                    ->add('locked', null, array('required' => false))
+                    ->add('expired', null, array('required' => false))
+                    ->add('enabled', null, array('required' => false))
+                    ->add('credentialsExpired', null, array('required' => false))
+                ->end()
+                ->with('Groups')
+                    ->add('groups', 'sonata_type_model', array(
+                        'required' => false,
+                        'expanded' => true,
+                        'multiple' => true,
+                    ))
+                ->end()
+                ->with('Roles')
+                    ->add('realRoles', 'sonata_security_roles', array(
+                        'label' => 'form.label_roles',
+                        'expanded' => true,
+                        'multiple' => true,
+                        'required' => false,
+                    ))
+                ->end()
                 ->with('Keys')
                     ->add('token', null, array('required' => false))
                     ->add('twoStepVerificationCode', null, array('required' => false))
